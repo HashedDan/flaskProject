d41edcc02f4c85f2f153f94ebb2d89f713467aee
HashedDan <dmangum@hashedhealth.com>
2017-11-03 14:41:31 -0500

    Basic routing setup

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
+from flask import render_template, flash, redirect
 from app import app
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+    return render_template("base.html", title='Home')
+
+@app.route('/login')
+def login():
+	return render_template("login.html", title='Log In')
+
+@app.route('/signup')
+def signup():
+	return render_template("signup.html", title='Sign Up')
